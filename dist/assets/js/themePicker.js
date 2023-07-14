9497d0d38920d454d4a0d88447737b20e1219dd1
Ed Millington <ed@timezoneone.com>
2023-07-14 15:47:19 -0700

    Save selected theme to local storage

--- dist/assets/js/themePicker.js
+++ dist/assets/js/themePicker.js
@@ -2,24 +2,33 @@ const themePicker = document.getElementById('js-theme-picker');
 
 themePicker.addEventListener('change', function changeTheme(event){
 
-	for (let i = 0; i < event.target.options.length; i++) {
-
-		document.body.classList.remove(event.target.options[i].value);
-
-	}
-
 	setTheme(event.target.value);
 
 });
 
 function setTheme(value) {
-	sessionStorage.setItem('edmill-theme', value);
+
+	localStorage.setItem('edmill-theme', value);
+
+	for (let i = 0; i < themePicker.options.length; i++) {
+
+		document.body.classList.remove(themePicker.options[i].value);
+
+	}
+
 	document.body.classList.add(value);
 };
 
 const theme = localStorage.getItem('edmill-theme');
 
 if (theme) {
-	sessionStorage.value = theme;
+
+	themePicker.value = theme;
+
 	setTheme(theme);
+
+} else {
+
+	setTheme(themePicker.value);
+
 };
