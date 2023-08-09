858785f3340e1434c4c6e2d875236b39b16de9d4
Ed Millington <ed@timezoneone.com>
2023-08-09 14:34:53 -0700

    add site icon

--- src/js/site/src/themePicker.js
+++ src/js/site/src/themePicker.js
@@ -1,39 +1,39 @@
+function setTheme(value, themePickerEl) {
+
+    localStorage.setItem('edmill-theme', value);
+
+    for (let i = 0; i < themePickerEl.options.length; i++) {
+
+        document.body.classList.remove(themePickerEl.options[i].value);
+
+    }
+
+    document.body.classList.add(value);
+
+}
 
 export function themePicker() {
 
-	const themePickerEl = document.getElementById('js-theme-picker');
+    const themePickerEl = document.getElementById('js-theme-picker');
 
-	themePickerEl.addEventListener('change', function changeTheme(event){
+    themePickerEl.addEventListener('change', function changeTheme(event) {
 
-		setTheme(event.target.value);
+        setTheme(event.target.value, themePickerEl);
 
-	});
+    });
 
-	function setTheme(value) {
+    const theme = localStorage.getItem('edmill-theme');
 
-		localStorage.setItem('edmill-theme', value);
+    if (theme) {
 
-		for (let i = 0; i < themePickerEl.options.length; i++) {
+        themePickerEl.value = theme;
 
-			document.body.classList.remove(themePickerEl.options[i].value);
+        setTheme(theme, themePickerEl);
 
-		}
+    } else {
 
-		document.body.classList.add(value);
-	};
+        setTheme(themePickerEl.value, themePickerEl);
 
-	const theme = localStorage.getItem('edmill-theme');
-
-	if (theme) {
-
-		themePickerEl.value = theme;
-
-		setTheme(theme);
-
-	} else {
-
-		setTheme(themePickerEl.value);
-
-	};
+    }
 
 }
